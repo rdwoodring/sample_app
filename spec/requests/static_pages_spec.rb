0549536dff57d5d742c4b8f034608698cc0ba1e8
Ryan Woodring <rdwoodring@gmail.com>
2012-12-01 15:40:20 -0500

    Finish static page layout and update routing

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,15 +5,15 @@ describe "Static pages" do
 	describe "Home page" do
 
 		it "should have the content 'Sample App'" do
-			visit '/static_pages/home'
+			visit root_path
 			page.should have_selector('h1', :text =>'Sample App')
 		end
 		it "should have the base title" do
-			visit '/static_pages/home'
+			visit root_path
 			page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App")
 		end
 		it "should not have a custom page title" do
-			visit '/static_pages/home'
+			visit root_path
 			page.should_not have_selector('title', :text => "| Home")
 		end
 	end
@@ -21,11 +21,11 @@ describe "Static pages" do
 	describe "Help page" do
   
 		it "should have the content 'Help'" do
-			visit '/static_pages/help'
+			visit help_path
 			page.should have_selector('h1', :text => 'Help')
 		end
 		it "should have the right title" do
-			visit '/static_pages/help'
+			visit help_path
 			page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Help")
 		end
 	end
@@ -33,12 +33,25 @@ describe "Static pages" do
 	describe "About page" do
 		
 		it "should have the content 'About Us'" do
-			visit '/static_pages/about'
+			visit about_path
 			page.should have_selector('h1', :text =>'About Us')
 		end
 		it "should have the right title" do
-			visit '/static_pages/about'
+			visit about_path
 			page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | About Us")
+		end
+	end
+	
+	describe "Contact page" do
+	
+		it "should have the h1 'Contact'" do
+			visit contact_path
+			page.should have_selector('h1', text: 'Contact')
+		end
+		
+		it "should have the title 'Contact'" do
+			visit contact_path
+			page.should have_selector('title', text: "Ruby on Rails Tutorial Sample App | Contact")
 		end
 	end
 end
